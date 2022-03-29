14e050ca6c1e5a5d4c3ff944be05fa8df20358ef
David Delgado <david.delgadop88@gmail.com>
2022-03-29 14:04:47 -0400

    turning into flip card

---
+++ script.js
@@ -0,0 +1,6 @@
+const submitButton = document.getElementById('submit-btn');
+const ratingCard = document.querySelector('.rating-card');
+const thankYouCard = document.querySelector('.thankyou-card');
+const rating = document.querySelector('input[name=rating]:checked');
+
+console.log(rating);
